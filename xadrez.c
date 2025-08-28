66d182d9c8f7b3f3ceead44aae3c950d813c1a63
Mateus2077lima <118564563+Mateus2077lima@users.noreply.github.com>
2025-08-28 19:09:15 +0000

    Codigo Nivel Mestre finalizado

--- xadrez.c
+++ xadrez.c
@@ -1,177 +1,105 @@
 #include <stdio.h>
 
+void movimentoTorre(int casa){
+    if (casa > 0){
+        movimentoTorre(casa - 1);
+        printf ("Casa %d - direita \n", casa);
+    }
+    
+}
+
+void movimentoBispo(int casas){
+    if(casas > 0){
+        movimentoBispo(casas - 1);
+
+        int linha = casas; // loop externo
+        // loop interno só percorre a coluna diagonal
+        for(int coluna = linha; coluna <= linha; coluna++){
+            printf("Bispo -> Casa (%d,%d) - Cima e Direita\n", linha, coluna);
+        }
+    }
+}
+
+
+void movimentoRainha(int casa){
+    if (casa > 0){
+        movimentoRainha(casa - 1);
+        printf("Casa %d - Esquerda \n",casa);
+    }
+}
+
 int main(){
     //Declaração das variaveis
-    int i = 1, j = 1;
-    int posicaoCasa = 1;
-    int numeroMovimentos,opcao_1,opcao_2;
+    int numeroMovimentos, contador;
 
-    //lendo o número de movimentos que o usuario quer que a torre faça
     printf("Atenção digitar somente até 8 movimentos!!! \n");
     printf("Digite a quantidade de movimentos da torre: ");
     scanf("%d", &numeroMovimentos);
 
-    //utilizando do-while para fazer os movimentos da torre
+    printf("=================================\n");
+    printf("         Movimento da Torre      \n");
+    printf("=================================\n");
+
     if(numeroMovimentos <= 8){
-        do{
-            if (i == 1){
-                printf("=================================\n");
-                printf("         Movimento da Torre      \n");
-                printf("=================================\n");
-            }
-            
-            printf("Casa %d - Direita \n",posicaoCasa);
-            i++;
-            posicaoCasa++;
-        } while (i <= numeroMovimentos);
-    }else {
-        printf("Número de movimentos invalido!!! \n");
-    }
+        movimentoTorre(numeroMovimentos);
+    } else {
+        printf("Número de movimentos invalido");
+    }       
     
 
-    i = 1;//reiniciando a variavel i
-    posicaoCasa = 1;//reiniciando a variavel posicaoCasa
     printf("=================================\n");
     //lendo o número de movimentos que o usuario quer que a bispo faça
     printf("Atenção digitar somente até 8 movimentos!!! \n");
     printf("Digite a quantidade de movimentos do bispo: ");
     scanf("%d", &numeroMovimentos);
-    if (numeroMovimentos <= 8){
-    
-        while (i <= numeroMovimentos){
-            if (i == 1){
-                printf("\n=================================\n");
-                printf("         Movimento do Bispo      \n");
-                printf("=================================\n");
-            }
 
-            printf("Casa %d - Cima,Direita \n", posicaoCasa);
-            i++;
-            posicaoCasa++;
-        }
+    printf("\n=================================\n");
+    printf("         Movimento do Bispo      \n");
+    printf("=================================\n");
+
+    if (numeroMovimentos <= 8){
+        movimentoBispo(numeroMovimentos);
     } else {
         printf("Número de movimentos invalido!!! \n");
     }
 
     printf("=================================\n");
-    posicaoCasa = 1;//reiniciando a variavel posição casa;
     //lendo o número de movimentos que o usuario quer que a rainha faça
     printf("Atenção digitar somente até 8 movimentos!!! \n");
     printf("Digite a quantidade de movimentos da rainha: ");
     scanf("%d", &numeroMovimentos);
 
+    printf("\n=================================\n");
+    printf("        Movimento da Rainha      \n");
+    printf("=================================\n");
+
     if(numeroMovimentos <= 8){
-        for (i = 1; i <= numeroMovimentos; i++){
-            if (i == 1){
-                printf("\n=================================\n");
-                printf("        Movimento da Rainha      \n");
-                printf("=================================\n");
-            }
-            
-            printf("Casa %d - Esquerda \n", posicaoCasa);
-            posicaoCasa++;
-        }
+        movimentoRainha(numeroMovimentos);
     } else {
         printf("Número de movimentos invalido!!! \n");
     }
 
-    printf("=================================\n");
     printf("\n=================================\n");
     printf("        Movimento do Cavalo      \n");
     printf("=================================\n");
 
-    //Escolha do usuario para onde o cavalo vai se mover
-
-    printf("Escolha uma opção para a direção que o cavalo deve se mover: \n");
-    printf("1.Esquerda \n");
-    printf("2.Direita \n");
-    printf("Escolha: ");
-    scanf("%d", &opcao_1);
-
-    printf("Escolha uma opção para a direção que o cavalo deve se mover: \n");
-    printf("1.Cima \n");
-    printf("2.Baixo \n");
-    printf("Escolha: ");
-    scanf("%d", &opcao_2);
-
-    int k = 1;//inicialização da variavel k
-
-    //utilizado switch para percorrer as decisões do usuario
-    switch (opcao_1){
-    case 1:
-        switch (opcao_2){
-        case 1://mostra pro usuario cima,cima,esquerda
-            for (i = 1; i == 1; i--){
-                while (j <= 2){
-                    printf("Cima \n");
-                    j++;
-                }
-            }
-
-            do{
-                printf("Esquerda \n");
-                k--;
-            } while (k == 1);
-        break;
-        case 2://mostra pro usuario baixo,baixo,esquerda
-            for (i = 1; i == 1; i--){
-                while (j <= 2){
-                    printf("Baixo \n");
-                    j++;
-                }
-            }
-
-            do{
-                printf("Esquerda \n");
-                k--;
-            } while (k == 1);
-        break;
-        
-        default:
-            printf("Opção inválida");
-        break;
-        }
-    break;
-    case 2:
-        switch (opcao_2){
-        case 1://mostra pro usuario cima,cima,direita
-            for (i = 1; i == 1; i--){
-                while (j <= 2){
-                    printf("Cima \n");
-                    j++;
-                }
-            }
-
-
-            do{
-                printf("Direita \n");
-                k--;
-            } while (k == 1);
-        break;
-        case 2://mostra pro usuario baixo,baixo,esquerda
-            for (i = 1; i == 1; i--){
-                while (j <= 2){
-                    printf("Baixo \n");
-                    j++;
-                }
-            }
-
-            do{
-                printf("Direita \n");
-                k--;
-            } while (k == 1);
-        break;
-        
-        default:
-            printf("Opção inválida");
-        break;
-        }
-    break;
+    numeroMovimentos = 2;
+    contador = 1;
     
-    default:
-        printf("Opção inválida");
-    break;
+
+    for (int i = 1; i <= numeroMovimentos; i++){
+        for (int j = 1; j <= numeroMovimentos; j++){
+            if(i == j) continue;
+            if (contador > numeroMovimentos) break;
+
+            printf("Movimento %d: Cima, Cima, Direita\n", contador);
+            contador++;
+            
+        }
+        
     }
+    
+   
     
     
     printf("=================================\n");
@@ -179,11 +107,3 @@ int main(){
      
     return 0;
 }
-
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
